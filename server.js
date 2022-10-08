0dc8a86650e4df8d59695d5598d5231dd344bce8
Marwa <koroko123430@gmail.com>
2022-10-08 20:43:07 +0300

    s

--- server.js
+++ server.js
@@ -1,20 +1,9 @@
-var express = require('express'),
-    app = express();
-
-app.use(express.static('www'));
-
-// CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
-app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next();
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/www'));
+// Link index.html of build folder with router.
+app.get('/*', (req, res, next) => {
+    res.sendFile(path.join(__dirname + '/www/index.html'));
 });
-
-// API Routes
-// app.get('/blah', routeHandler);
-
-app.set('port', process.env.PORT || 5000);
-
-app.listen(app.get('port'), function() {
-    console.log('Express server listening on port ' + app.get('port'));
-});
+app.listen(process.env.PORT || 8000);
